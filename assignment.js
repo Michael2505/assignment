c221fb4c3e44416494237399edabe2c9f93a4cc5
Michael2505 <michaelanegbe@gmail.com>
2022-11-16 13:51:32 +0100

    first commit to github

---
+++ assignment.js
@@ -0,0 +1,10 @@
+let age = 5
+let voteable = (age %2==0) ? "even":"old";
+console.log(voteable);
+
+let flower = "rose"
+let tree = "apple"
+let newflower = flower.replace( "rose" , "apple");
+let newtree = tree.replace("apple", "rose");
+console.log(newflower);
+console.log(newtree);
